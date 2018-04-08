b10948a00d97dbc41998cef11c9c016f503fa130
James Armetta <jamesarmetta@users.noreply.github.com>
2018-04-08 13:09:47 -0400

    update
    cleaned up code
    number of swaps
    total sorts prefomed

--- Test Sort/Test Sort/TestSort.cs
+++ Test Sort/Test Sort/TestSort.cs
@@ -10,17 +10,11 @@ using System;
 namespace TestSort
 {
     internal class TestSort
-    {
-        private int numberOfSorts = 1;
-        private int numberOfPreformedSorts = 0;
-        private int sizeTestArray = 0;
-        private int success = 1;
-        private int failure = 1;
+    {      
+        public int success = 1;
+        public int failure = 1;
 
-        //public static int GetIntFromUser(int minVal, int maxVal);
-        //public static int[] GetRandomizedArray(int size, int minRndVal, int maxRndVal);
-        //int[] randomValues = GetRandomizedArray(GetIntFromUser(1, 1000), 1, 1000);
-        public static void ArraySize(int[] array) //User input for the array size
+        public static void sizeTestArray(int[] array) //User input for the array size
         {
             Console.Write("Enter array size: ");
             int n;
@@ -37,7 +31,7 @@ namespace TestSort
             {
                 newArray[i] = rand.Next(1, 1000);
             }
-            Console.WriteLine("The Array Before Selection Sort is: ");
+            Console.WriteLine("Here is the unsorted array: ");
             Console.WriteLine(" ");
             for (int i = 0; i < array.Length; ++i)
             {
@@ -46,27 +40,11 @@ namespace TestSort
             Console.WriteLine();
             Console.WriteLine();
         }
-        /*
-         static void PickArray(int[] array) //User input for the array size
-         {
-             int[] newArray = new int[n];
-             for (int i = 0; i < array.Length; i++)
-             {
-                 Console.Write("Enter array size: ");
-                 string input = Console.ReadLine();
-                 if (!int.TryParse(input, out array[i]))
-                 {
-                     // Tell the user something went wrong:
-                     Console.WriteLine("Sorry, '{0}' is not a valid number.", input);
-                     // Don't move to the next array element:
-                     i--;
-                 }
-             }
-         }
-         */
-        public static void Sort(int[] arr) //Sorts array using selection sort
+        public static void SelectionSort(int[] arr) //Sorts array using selection sort
         {
-            Console.WriteLine("The Array After Selection Sort is: ");
+            int numberOfSorts = 1;
+
+            Console.WriteLine("Here is the sorted array: ");
             Console.WriteLine();
             int n = arr.Length;
             // One by one move boundary of unsorted subarray
@@ -82,12 +60,10 @@ namespace TestSort
                 int temp = arr[min_idx];
                 arr[min_idx] = arr[i];
                 arr[i] = temp;
+                numberOfSorts++;
             }
-        }
-        // Prints the original array
-        public static void printArray(int[] arr)
-        {
-
+            Console.WriteLine("Number of Swaps: " + numberOfSorts);
+            Console.WriteLine();
         }
         // Prints the sorted array
         public static void printSorted(int[] arr)
@@ -98,8 +74,11 @@ namespace TestSort
             Console.WriteLine();
         }
         // Driver code
+
         public static void Main()
-        {          
+        {
+            int numberOfPreformedSorts = 0;
+
             do
             {
                 Console.WriteLine("-----------------------------------------------------------------------------------------------------");
@@ -114,25 +93,37 @@ namespace TestSort
                     Console.WriteLine("Pick an array size between 1 and 1,000");
 
                     int Min = 1;
-                    int Max = 200;
+                    int Max = 1000;
 
-                    int[] arr = new int[100];
+                    int[] arr = new int[10];
 
                     Random randNum = new Random();
                     for (int i = 0; i < arr.Length; i++)
                     {
                         arr[i] = randNum.Next(Min, Max);
                     }
-                    ArraySize(arr);
-                    Sort(arr);
-                    
+                    sizeTestArray(arr);
+                    SelectionSort(arr);
+                    if (arr == null)
+                    {
+                        Console.WriteLine("Failure: ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Succsess: ");
+                    }
                     printSorted(arr);
+
+                    numberOfPreformedSorts++;
+                    Console.WriteLine();
+                    Console.WriteLine("Number of Sorts Preformed: " + numberOfPreformedSorts);
+
                 }
                 else if (Console.ReadKey().Key != ConsoleKey.NumPad2) //what happens when 2 is pressed
                 {
                     Console.WriteLine("Exit");
                 }
-            } while (true);
+            } while (numberOfPreformedSorts != 4;
         }
     }
 }
